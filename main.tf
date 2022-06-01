401d40953fadb3b259dc4fc0b40e4f736fadfde1
ryota0624 <tennis.suta6@gmail.com>
2022-06-01 10:53:28 +0900

    iam roleの定義をmodule化

--- main.tf
+++ main.tf
@@ -57,6 +57,15 @@ provider "aws" {
 #   }
 # }
 
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+
 module "web_server" {
   source = "./http_server"
   instance_type = "t3.micro"
@@ -64,4 +73,12 @@ module "web_server" {
 
 output "public_dns" {
   value = module.web_server.public_dns
+}
+
+
+module "describe_regions_for_ec2" {
+  source = "./iam_role"
+  name = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
 }

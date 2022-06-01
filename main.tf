744cba255e89bcb65ca5a3d39d14041e45c3a51b
ryota0624 <tennis.suta6@gmail.com>
2022-06-01 15:51:46 +0900

    security group定義のmodule化

--- main.tf
+++ main.tf
@@ -172,7 +172,7 @@ resource "aws_vpc" "example" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = {
+  tags                 = {
     Name = "example"
   }
 }
@@ -245,14 +245,14 @@ resource "aws_route_table_association" "private_1" {
 
 
 resource "aws_eip" "nat_gateway_0" {
-  vpc = true
+  vpc        = true
   depends_on = [
     aws_internet_gateway.example
   ]
 }
 
 resource "aws_eip" "nat_gateway_1" {
-  vpc = true
+  vpc        = true
   depends_on = [
     aws_internet_gateway.example
   ]
@@ -261,7 +261,7 @@ resource "aws_eip" "nat_gateway_1" {
 resource "aws_nat_gateway" "nat_gateway_0" {
   allocation_id = aws_eip.nat_gateway_0.id
   subnet_id     = aws_subnet.public_0.id
-  depends_on = [
+  depends_on    = [
     aws_internet_gateway.example
   ]
 }
@@ -269,7 +269,7 @@ resource "aws_nat_gateway" "nat_gateway_0" {
 resource "aws_nat_gateway" "nat_gateway_1" {
   allocation_id = aws_eip.nat_gateway_1.id
   subnet_id     = aws_subnet.public_1.id
-  depends_on = [
+  depends_on    = [
     aws_internet_gateway.example
   ]
 }
@@ -289,40 +289,47 @@ resource "aws_route" "private_1" {
 
 
 resource "aws_subnet" "public_0" {
-  vpc_id = aws_vpc.example.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_1" {
-  vpc_id = aws_vpc.example.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "ap-northeast-1c"
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
   map_public_ip_on_launch = true
 }
 
 resource "aws_security_group" "example" {
-  name = "example"
-  vpc_id = aws_vpc.example.id  
+  name   = "example"
+  vpc_id = aws_vpc.example.id
 }
 
 resource "aws_security_group_rule" "ingress_example" {
-  type = "ingress"
-  from_port = "80"
-  to_port = "80"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.example.id
 }
 
 
 resource "aws_security_group_rule" "egress_example" {
-  type = "ingress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.example.id
 }
 
+module "example_sg" {
+  source     = "./security_group"
+  name       = "module-sg"
+  vpc_id     = aws_vpc.example.id
+  port       = 80
+  cidr_block = ["0.0.0.0/0"]
+}

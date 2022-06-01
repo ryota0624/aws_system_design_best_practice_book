4cf8ae7f266228aade83690b5e7446d660125d34
ryota0624 <tennis.suta6@gmail.com>
2022-06-01 14:38:25 +0900

    alb log用のbucketを定義

--- main.tf
+++ main.tf
@@ -131,3 +131,35 @@ resource "aws_s3_bucket_cors_configuration" "public" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "alb-log" {
+  bucket = aws_s3_bucket.alb_log.id
+  rule {
+    id = "rule-1"
+    expiration {
+      days = "180"
+    }
+    status = "Enabled"
+  }
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = [ "arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*" ]
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}

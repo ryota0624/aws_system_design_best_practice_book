0aa72d44e294eecb93a1bec09ebdd27a1c806fc6
ryota0624 <tennis.suta6@gmail.com>
2022-06-01 15:42:49 +0900

    security_groupの定義

--- main.tf
+++ main.tf
@@ -302,4 +302,27 @@ resource "aws_subnet" "public_1" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_security_group" "example" {
+  name = "example"
+  vpc_id = aws_vpc.example.id  
+}
+
+resource "aws_security_group_rule" "ingress_example" {
+  type = "ingress"
+  from_port = "80"
+  to_port = "80"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+
+resource "aws_security_group_rule" "egress_example" {
+  type = "ingress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
 

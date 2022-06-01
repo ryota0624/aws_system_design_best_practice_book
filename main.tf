6cf0581941e49d10e65ec893bb1fc4f5957cf70f
ryota0624 <tennis.suta6@gmail.com>
2022-06-01 11:51:27 +0900

    format

--- main.tf
+++ main.tf
@@ -59,15 +59,15 @@ provider "aws" {
 
 data "aws_iam_policy_document" "allow_describe_regions" {
   statement {
-    effect = "Allow"
-    actions = ["ec2:DescribeRegions"]
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
     resources = ["*"]
   }
 }
 
 
 module "web_server" {
-  source = "./http_server"
+  source        = "./http_server"
   instance_type = "t3.micro"
 }
 
@@ -77,8 +77,8 @@ output "public_dns" {
 
 
 module "describe_regions_for_ec2" {
-  source = "./iam_role"
-  name = "describe-regions-for-ec2"
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
   identifier = "ec2.amazonaws.com"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
